e73d9993f5bc731ce7cff2068c68cb51e8d1f48d
Jack_Wu <dm3352andy@gmail.com>
2021-09-30 04:53:32 +0800

    Refactor

--- app/src/main/java/com/jack/qrcodefor1922/MainActivity.kt
+++ app/src/main/java/com/jack/qrcodefor1922/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.jack.qrcodefor1922
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.*
 import android.content.pm.PackageManager
 import android.net.Uri
@@ -22,14 +21,9 @@ import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.view.setPadding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.mlkit.vision.barcode.Barcode
-import com.google.mlkit.vision.barcode.BarcodeScannerOptions
-import com.google.mlkit.vision.barcode.BarcodeScanning
-import com.google.mlkit.vision.common.InputImage
-import kotlinx.android.synthetic.main.activity_main2.*
-import java.io.File
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicBoolean
@@ -39,38 +33,74 @@ typealias QRCodeListener = (barcodes: List<Barcode>) -> Unit
 
 class MainActivity2 : AppCompatActivity() {
 
-    private val PREF_FRAGMENT_TAG = "settings"
+    companion object {
+        private const val FRAGMENT_TAG_SETTINGS = "settings"
+        private const val AGREEMENT = "agreement"
+        const val PREFKEY = "1922qrcode"
+        private const val TAG = "QRCodeFor1922_new_api"
+        private const val VAILD_NUMBER = "1922"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private val VIBRATE_PATTERN = longArrayOf(500, 500)
 
-    // porting from MainActivity
-    private var mLastText: String = ""
-    private var mAgreement = false
-    private var bDialogShowing = false
-    private var mWithFamilyNum = 0
+
+        private const val PREF_CLOSE_APP_AFTER_SCAN = "close_after_scan"
+        private const val PREF_VIBRATE_WHEN_SUCCESS = "vibrate_when_success"
+        private const val PREF_AUTO_OPEN_SCHEMA = "auto_open_identify_schema"
+        private const val PREF_AUTO_COPY_TEXT = "auto_copy_non_1922"
+        private const val PREF_COPY_TEXT_VIBRATE = "vibrate_when_copy_text_success"
+
+        // Wait a mount of time. If no 1922 number then trigger first QRCode
+        private const val MSG_FORCE_TRIGGER = 1
+
+        // Time interval to trigger next QRCode
+        private const val MSG_COOLING_TIME = 2
+        private val mForeTrigger = AtomicBoolean(false)
+    }
+
+    private var mLastTriggerText: String = ""
+    private var bAgreement = false
+    private var bRedirectDialogShowing = false
+    private var mAccompanyNum = 0
+
     private lateinit var mPref: SharedPreferences
     private lateinit var mBgThread: HandlerThread
     private lateinit var REQUIRED_PERMISSIONS : Array<String>
     private lateinit var mHandler: BgHandler
-
-
-    private lateinit var outputDirectory: File
     private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main)
+
         REQUIRED_PERMISSIONS = if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
             arrayOf(Manifest.permission.CAMERA, Manifest.permission.SEND_SMS)
         } else {
             arrayOf(Manifest.permission.CAMERA, Manifest.permission.SEND_SMS,
                 Manifest.permission.READ_PHONE_STATE)
         }
+
         try {
             mPref = getSharedPreferences(PREFKEY, MODE_PRIVATE)
         } catch (e: Exception) {
         }
 
-        mAgreement = mPref.getBoolean(AGREEMENT, false)
-        if (!mAgreement) {
+        // Show agreement at first open
+        showAgreement()
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        // Using another thread to delay trigger QRCode
+        mBgThread = HandlerThread("timer")
+        mBgThread.start()
+        mHandler = BgHandler(mBgThread.looper)
+
+        initAccompanyView()
+    }
+
+    private fun showAgreement() {
+        bAgreement = mPref.getBoolean(AGREEMENT, false)
+        if (!bAgreement) {
             val builder = AlertDialog.Builder(this)
             builder.setCancelable(false)
                 .setTitle(getString(R.string.claim_title))
@@ -80,13 +110,7 @@ class MainActivity2 : AppCompatActivity() {
                 ) { _, _ ->
                     mPref.edit().putBoolean(AGREEMENT, true).apply()
                     // Request camera permissions
-                    if (allPermissionsGranted()) {
-                        startCamera()
-                    } else {
-                        ActivityCompat.requestPermissions(
-                            this@MainActivity2, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
-                        )
-                    }
+                    startCamera()
                 }
                 .setNegativeButton(
                     getString(R.string.not_agree)
@@ -95,41 +119,34 @@ class MainActivity2 : AppCompatActivity() {
                 }
                 .show()
         } else {
-            // Request camera permissions
-            if (allPermissionsGranted()) {
-                startCamera()
-            } else {
-                ActivityCompat.requestPermissions(
-                    this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
-                )
-            }
+            startCamera()
         }
-
-        outputDirectory = getOutputDirectory()
-
-        cameraExecutor = Executors.newSingleThreadExecutor()
-
-        mBgThread = HandlerThread("timer")
-        mBgThread.start()
-        mHandler = BgHandler(mBgThread.looper)
-        initWithFamily()
-
     }
 
-    private fun initWithFamily() {
+    private fun startCamera() {
+        if (allPermissionsGranted()) {
+            startCameraLock()
+        } else {
+            ActivityCompat.requestPermissions(
+                this@MainActivity2, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+    }
+
+    private fun initAccompanyView() {
         val bar = findViewById<SeekBar>(R.id.with_family_bar)
         val view = findViewById<TextView>(R.id.with_family_view)
         val plus = findViewById<TextView>(R.id.plus_sign)
         view.setOnClickListener {
-            showEdittextDialog(view)
+            showEnterNumberDialog(view)
         }
         plus.setOnClickListener {
-            showEdittextDialog(view)
+            showEnterNumberDialog(view)
         }
-        bar.setOnSeekBarChangeListener(object :SeekBar.OnSeekBarChangeListener{
+        bar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                mWithFamilyNum = progress
-                view.text = mWithFamilyNum.toString()
+                mAccompanyNum = progress
+                view.text = mAccompanyNum.toString()
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
@@ -141,9 +158,13 @@ class MainActivity2 : AppCompatActivity() {
         })
     }
 
-    private fun showEdittextDialog(view: TextView) {
+    /**
+     * Let user enter accompany number
+     */
+    private fun showEnterNumberDialog(view: TextView) {
         val editText = EditText(this)
         editText.inputType = InputType.TYPE_CLASS_NUMBER
+        // Limit input length
         val filter = InputFilter.LengthFilter(2)
         editText.filters = arrayOf(filter)
 
@@ -151,19 +172,20 @@ class MainActivity2 : AppCompatActivity() {
             .setCancelable(false)
             .setTitle(getString(R.string.enter_accompany_num))
             .setView(editText)
-            .setPositiveButton(getString(R.string.dialog_confirm)){_,_->
+            .setPositiveButton(getString(R.string.dialog_confirm)) { _, _ ->
                 editText.let {
                     if (it.text.isNotEmpty()) {
                         var num = it.text.toString().toInt()
                         if (num > 10) num = 10
-                        mWithFamilyNum = num
+                        mAccompanyNum = num
                     } else {
-                        mWithFamilyNum = 0
+                        mAccompanyNum = 0
                     }
-                    view.text = mWithFamilyNum.toString()
+                    view.text = mAccompanyNum.toString()
                 }
             }.show()
 
+        // Keyboard not showing when dialog show. Trigger manually.
         editText.postDelayed({
             editText.requestFocus()
             val input = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
@@ -172,7 +194,7 @@ class MainActivity2 : AppCompatActivity() {
 
     }
 
-    private fun startCamera() {
+    private fun startCameraLock() {
 
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
@@ -246,37 +268,9 @@ class MainActivity2 : AppCompatActivity() {
         ) == PackageManager.PERMISSION_GRANTED
     }
 
-    private fun getOutputDirectory(): File {
-        val mediaDir = externalMediaDirs.firstOrNull()?.let {
-            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
-        }
-        return if (mediaDir != null && mediaDir.exists())
-            mediaDir else filesDir
-    }
-
     override fun onDestroy() {
         super.onDestroy()
         cameraExecutor.shutdown()
-    }
-
-    companion object {
-        private const val AGREEMENT = "agreement"
-        const val PREFKEY = "1922qrcode"
-        private const val TAG = "QRCodeFor1922_new_api"
-        private const val REQUEST_CODE_PERMISSIONS = 10
-
-        // porting from MainActivity
-        private val VIBRATE_PATTERN = longArrayOf(500, 500)
-        private val PREFIX = "smsto"
-        private val VAILD_NUMBER = "1922"
-        private const val PREF_CLOSE_APP_AFTER_SCAN = "close_after_scan"
-        private const val PREF_VIBRATE_WHEN_SUCCESS = "vibrate_when_success"
-        private const val PREF_AUTO_OPEN_SCHEMA = "auto_open_identify_schema"
-        private const val PREF_AUTO_COPY_TEXT = "auto_copy_non_1922"
-        private const val PREF_COPY_TEXT_VIBRATE = "vibrate_when_copy_text_success"
-        private val MSG_FORCE_TRIGGER = 1
-        private val MSG_COOLING_TIME = 2
-        private val mForeTrigger = AtomicBoolean(false)
     }
 
     override fun onRequestPermissionsResult(
@@ -286,51 +280,15 @@ class MainActivity2 : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
             if (allPermissionsGranted()) {
-                startCamera()
+                startCameraLock()
             } else {
-                Toast.makeText(
-                    this,
-                    "Permissions not granted by the user.",
-                    Toast.LENGTH_SHORT
-                ).show()
-                finish()
+                AlertDialog.Builder(this)
+                    .setTitle(getString(R.string.hint_permission_title))
+                    .setMessage(getString(R.string.hint_permission_mes))
+                    .setPositiveButton(getString(R.string.dialog_confirm)) { _, _ ->
+                        finish()
+                    }.show()
             }
-        }
-    }
-
-    private class QRCodeAnalyzer(private val listener: QRCodeListener) : ImageAnalysis.Analyzer {
-        val options = BarcodeScannerOptions.Builder()
-            .setBarcodeFormats(
-                Barcode.FORMAT_QR_CODE, Barcode.FORMAT_AZTEC
-            )
-            .build()
-        val scanner = BarcodeScanning.getClient(options)
-
-        @SuppressLint("UnsafeOptInUsageError")
-        override fun analyze(imageProxy: ImageProxy) {
-
-
-            val mediaImage = imageProxy.image
-            if (mediaImage != null) {
-                val image =
-                    InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
-
-//                Log.d(TAG, "${image.width} ${image.height}")
-                scanner.process(image)
-                    .addOnSuccessListener { barcodes ->
-                        listener(barcodes)
-                    }
-                    .addOnFailureListener {
-                        // Task failed with an exception
-                        // ...
-
-                    }
-                    .addOnCompleteListener {
-                        imageProxy.close()
-                    }
-
-            }
-
         }
     }
 
@@ -347,11 +305,11 @@ class MainActivity2 : AppCompatActivity() {
                 startActivity(intent)
             }
             R.id.settings -> {
-                if (supportFragmentManager.findFragmentByTag(PREF_FRAGMENT_TAG) == null) {
+                if (supportFragmentManager.findFragmentByTag(FRAGMENT_TAG_SETTINGS) == null) {
                     supportFragmentManager
                         .beginTransaction()
-                        .replace(R.id.fragment_pref, SettingsPreference(), PREF_FRAGMENT_TAG)
-                        .addToBackStack(PREF_FRAGMENT_TAG)
+                        .replace(R.id.fragment_pref, SettingsPreference(), FRAGMENT_TAG_SETTINGS)
+                        .addToBackStack(FRAGMENT_TAG_SETTINGS)
                         .commit()
                 }
             }
@@ -359,25 +317,26 @@ class MainActivity2 : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-    val callback = object : QRCodeListener {
+    private val callback = object : QRCodeListener {
         override fun invoke(barcodes: List<Barcode>) {
             if (barcodes.isNotEmpty()) {
                 if (!mHandler.hasMessages(MSG_FORCE_TRIGGER)) {
                     mHandler.sendEmptyMessageDelayed(MSG_FORCE_TRIGGER, 1000)
                     mHandler.sendEmptyMessageDelayed(MSG_COOLING_TIME, 4000)
                 }
-                // If not force trigger then wait 1922 barcode in 1 second
                 if (!mForeTrigger.get()) {
+                    // Find 1922 number in list
                     for (barcode in barcodes) {
                         if (barcode.valueType == Barcode.TYPE_SMS
-                            && TextUtils.equals(barcode.sms?.phoneNumber, VAILD_NUMBER)) {
+                            && TextUtils.equals(barcode.sms?.phoneNumber, VAILD_NUMBER)
+                        ) {
                             mHandler.removeMessages(MSG_FORCE_TRIGGER)
                             mHandler.removeMessages(MSG_COOLING_TIME)
                             triggerBarcode(barcode)
                         }
                     }
                 } else {
-                    // Trigger directly
+                    // Trigger first QRCode directly
                     triggerBarcode(barcodes.first())
                 }
             }
@@ -387,7 +346,7 @@ class MainActivity2 : AppCompatActivity() {
     private class BgHandler(looper: Looper) : Handler(looper) {
         override fun handleMessage(msg: Message) {
             super.handleMessage(msg)
-            when(msg.what) {
+            when (msg.what) {
                 MSG_FORCE_TRIGGER -> {
                     mForeTrigger.set(true)
                 }
@@ -399,29 +358,29 @@ class MainActivity2 : AppCompatActivity() {
     }
 
     private fun triggerBarcode(barcode: Barcode) {
-        val showSettings = supportFragmentManager.findFragmentByTag(PREF_FRAGMENT_TAG)
+        val showSettings = supportFragmentManager.findFragmentByTag(FRAGMENT_TAG_SETTINGS)
             .takeIf { it != null }?.isVisible ?: false
-        if (barcode.rawValue == null || TextUtils.equals(mLastText, barcode.rawValue)
-            || showSettings || bDialogShowing){
+        if (barcode.rawValue == null || TextUtils.equals(mLastTriggerText, barcode.rawValue)
+            || showSettings || bRedirectDialogShowing
+        ) {
             return
         }
 
-        // Filter 1922
+        // Filter 1922 number
         if (barcode.valueType == Barcode.TYPE_SMS) {
-            // Vibrate
             if (mPref.getBoolean(PREF_VIBRATE_WHEN_SUCCESS, true)) {
                 vibrate()
             }
-            if (TextUtils.equals(barcode.sms.phoneNumber, VAILD_NUMBER)) {
+            if (TextUtils.equals(barcode.sms?.phoneNumber, VAILD_NUMBER)) {
                 val manager = SmsManager.getDefault()
                 var appendFamilyStr = ""
-                if (mWithFamilyNum != 0) {
-                    appendFamilyStr = "+$mWithFamilyNum"
+                if (mAccompanyNum != 0) {
+                    appendFamilyStr = "+$mAccompanyNum"
                 }
                 manager.sendTextMessage(barcode.sms.phoneNumber, null, "${barcode.sms.message} $appendFamilyStr", null, null)
                 val sendIntent = Intent(Intent.ACTION_SENDTO).apply {
                     type = "text/plain"
-                    data = Uri.parse("smsto:${barcode.sms.phoneNumber}")
+                    data = Uri.parse("smsto:${barcode.sms?.phoneNumber}")
                     putExtra("sms_body", "")
                 }
                 startActivity(sendIntent)
@@ -446,20 +405,23 @@ class MainActivity2 : AppCompatActivity() {
                     dialog.setPositiveButton(
                         getString(android.R.string.ok)
                     ) { dialog, which ->
-                        bDialogShowing = false
-                        startActivity(sendIntent) }
+                        bRedirectDialogShowing = false
+                        startActivity(sendIntent)
+                    }
                     dialog.setNeutralButton(
                         getString(R.string.copy_to_clipboard)
                     ) { dialog, which ->
-                        bDialogShowing = false
-                        copyToClipboard(barcode.rawValue) }
-                    dialog.setNegativeButton(android.R.string.cancel
-                    ) { _, _ -> bDialogShowing = false }
+                        bRedirectDialogShowing = false
+                        copyToClipboard(barcode.rawValue)
+                    }
+                    dialog.setNegativeButton(
+                        android.R.string.cancel
+                    ) { _, _ -> bRedirectDialogShowing = false }
                     dialog.setOnCancelListener {
-                        bDialogShowing = false
+                        bRedirectDialogShowing = false
                     }
                     dialog.show()
-                    bDialogShowing = true
+                    bRedirectDialogShowing = true
                 } else {
                     copyToClipboard(barcode.rawValue)
                 }
@@ -468,8 +430,8 @@ class MainActivity2 : AppCompatActivity() {
             }
         }
         mHandler.postDelayed(Runnable {
-            mLastText = ""
+            mLastTriggerText = ""
         }, 1500)
-        mLastText = barcode.rawValue
+        mLastTriggerText = barcode.rawValue
     }
 }
